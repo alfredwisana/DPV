4c53031b1219c56bafe173845980f2b88f87ea8e
alfredwisana <c14210177@john.petra.ac.id>
2024-05-06 08:00:34 +0700

    practical 1

---
+++ Practical 1/dataframe.py
@@ -0,0 +1,94 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# ------------------------------------------------------------
+# Creating SERIES
+# ------------------------------------------------------------
+
+# s1 = pd.Series([1, 2.56, "Petra", np.nan, True], name="c1")
+
+# s2 = pd.Series([1, 2.56, "Petra", np.nan, True], 
+#                index = ["aa", "bb", "cc", "dd", "ee"])
+
+# s3 = pd.Series({"day1": 420, "day2": 380, "day3": 390})
+
+# s4 = pd.Series({"day1": 420, "day2": 380, "day3": 390}, 
+#                index = ["day1", "day2"]) 
+
+# print('---------- S1 ----------')
+# print(s1)
+# print('\n---------- S2 ----------')
+# print(s2)
+# print('\n---------- S3 ----------')
+# print(s3)
+# print('\n---------- S4 ----------')
+# print(s4)
+# print('\n--------------------------')
+
+
+# ------------------------------------------------------------
+# SERIES Functions
+# ------------------------------------------------------------
+
+# # Hitung jumlah dari non-NaN values
+# print('Count =', s1.count())
+
+# # Hitung jumlah dari NaN values
+# print('NaN =', s1.isna().sum())
+
+# # Cetak frekuensi dari tiap non-NaN value
+# print('--------------------------')
+# print(s1.value_counts())
+
+# # Konversi series ke list
+# print('--------------------------')
+# print(s1.values.tolist())
+
+# # Hapus baris dengen index tertentu
+# s1_fix = s1.drop(index=[2,3])
+# s1_fix = s1.truncate(before=1, after=3)
+# print(s1_fix)
+# print('--------------------------')
+
+
+# ------------------------------------------------------------
+# Creating a DATAFRAME
+# ------------------------------------------------------------
+
+data = {
+    'c1' : ["qwe","asd","zxc"],
+    'c2' : [123,456,789],
+    'c3' : [False, True,True],
+    'c4' : [4,np.nan,6],
+}
+df_data = pd.DataFrame(data, index=['a','b','c'])
+print(df_data)
+
+# print('--------------------------')
+# print(df_data["c2"])
+# print('--------------------------')
+# print(df_data.iloc[0])
+# print('--------------------------')
+# df_data.dropna(inplace=True)
+# print(df_data)
+
+# # Mengecek apakah tiap baris memenuhi kondisi ttt.
+# print('--------------------------')
+# print(df_data['c2'] > 200)
+
+# # Simpan baris-baris di atas ke dataframe baru
+# print('--------------------------')
+# df_data2 = df_data[ df_data['c2'] > 200]
+# print(df_data2)
+
+
+
+# print('--------------------------')
+# cars = [["BMW", 3],["Volvo", 7],["Ford", 2]]
+# df_cars = pd.DataFrame(cars, 
+#                  index=["a",200,4.56], 
+#                  columns=["abc","def"])
+# print(df_cars)
+
---
+++ Practical 1/diabetes.py
@@ -0,0 +1,102 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+plt.rcParams.update({'font.size': 8, 'font.family': 'sans'})
+
+data = pd.read_csv('diabetes.tab.csv')
+print(data.head(20))
+
+
+# ----------------------------------------
+# HISTOGRAM PLOT
+# ----------------------------------------
+
+# Mempersiapkan empat tempat kosong untuk diagram 
+# Tempat setiap diagram diberi nama ax[0,0]; ax[0,1]; ax[1,0]; ax[1,1]
+# Sumbu Y setiap baris akan menggunakan aturan yg sama melalui sharey=True
+fig, ax = plt.subplots(nrows=2, ncols=2, sharex=False, sharey=False, 
+                       constrained_layout = True)  # dapat disingkat menjadi plt.subplots(2,2)
+fig.set_figheight(6)
+fig.set_figwidth(9)
+
+
+# menampilkan histogram utk BMI dengan lebar bin = 2, jumlah bins akan disesuaikan 
+sns.histplot(data, x='BMI', binwidth=2, stat='count', element='bars', palette='coolwarm', ax=ax[0,0])
+ax[0,0].set_title('Bin Width = 2')
+ax[0,0].set_xlabel('Body Mass Index (kg/m2)')
+ax[0,0].set_ylabel('Number of Patients')
+
+# menampilkan histogram utk BMI dengan jumlah bin = 15, lebar bin akan disesuaikan
+sns.histplot(data, x='BMI', bins=15, stat='frequency', element='step', ax=ax[0,1])
+ax[0,1].set_title('Number of bins = 15')
+ax[0,1].set_xlabel('Body Mass Index (kg/m2)')
+ax[0,0].set_ylabel('Frequency of the Patients')
+
+# menampilkan histogram utk AGE, garis KDE ditampilkan
+sns.histplot(data, x='AGE', stat='density', kde=True, ax=ax[1,0])
+ax[1,0].set_title('Age Density')
+ax[1,0].set_xlabel('Age of the patients')
+ax[1,0].set_ylabel('Density')
+
+# menampilkan histogram utk AGE dengan jumlah bin = 18, lebar bin akan disesuaikan
+sns.histplot(data, x='AGE', bins=18, stat='probability', ax=ax[1,1])
+ax[1,1].set_title('Age Probability')
+ax[1,1].set_xlabel('Age of the Patients')
+ax[1,1].set_ylabel('Probability')
+
+plt.suptitle('Histogram - Patients\' BMI Indexes and Age', fontsize=14, fontweight='bold')
+plt.show()
+
+
+# ----------------------------------------
+# DENSITY PLOT
+# ----------------------------------------
+
+# Dibuat subplot baru dengan sumbu Y dishare (memiliki setting yang sama)
+fig, ax = plt.subplots(3, 2, sharey=True, constrained_layout = True)
+fig.set_figheight(5.5)
+fig.set_figwidth(9)
+
+for i in range(0,3):
+    for j in range (0,2):
+        bw = round(0.2 + (i*0.4) + (j*0.2), 1)
+        if (j%2 != 0): fill=True 
+        else: fill=False
+        sns.kdeplot(data, x='AGE', bw_adjust=bw, fill=fill, ax=ax[i,j])
+        ax[i,j].set_title('Density Plot - Age with bandwidth=' + f'{bw}')
+        ax[i,j].set_xlabel('Age')
+plt.show()
+
+
+# ----------------------------------------
+# SCATTER PLOT
+# ----------------------------------------
+
+sns.set_theme(style="darkgrid")
+plt.figure(figsize=(10,6))
+sns.scatterplot(data, x='Y', y='BMI',size='AGE', hue='SEX', palette='coolwarm')  # Cari palette lain yang disediakan oleh matplotlib 
+plt.title('Progress of Diabetes and BMI Correlations', fontweight='bold')
+plt.ylabel('Diabetes Progress')
+plt.xlabel('BMI')
+plt.show()
+
+
+# ----------------------------------------
+# ECDF PLOT
+# ----------------------------------------
+
+sns.set_theme(style="whitegrid", palette="pastel")
+fig, ax = plt.subplots(1, 2, constrained_layout = True)
+fig.set_figheight(5.5)
+fig.set_figwidth(9)
+
+sns.ecdfplot(data, x='BMI', ax=ax[0])
+ax[0].set_title('Body Mass Index')
+sns.ecdfplot(data, x='BP', ax=ax[1])
+ax[1].set_title('Blood Pressure')
+
+plt.suptitle('ECDF of Various Attributes')
+plt.show()
