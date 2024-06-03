eef5c4cb066027f0032e5f3582678afb893c3fa5
Alfred Wisana <117831014+alfredwisana@users.noreply.github.com>
2024-06-03 10:10:34 +0700

    PVD 3

--- Practical 4/contoh_plots.py
+++ Practical 4/contoh_plots.py
@@ -16,6 +16,7 @@ pct_bedrooms = (housing_df.total_bedrooms / housing_df.total_rooms).agg('nanmean
 housing_df.total_bedrooms = housing_df.total_bedrooms.fillna(pct_bedrooms * housing_df.total_rooms)
 # ... median income menunjukkan angka seharusnya. Angka yg tersimpan di CSV dalam satuan USD10,000
 housing_df['median_income'] *= 10000
+
 # ... pastikan beberapa kolom dalam satuan yang tepat sebelum ditampilkan dalam plot
 housing_df = housing_df.astype({'housing_median_age':int, 'total_rooms':int,
                                 'total_bedrooms':int, 'population':int,
@@ -26,15 +27,20 @@ housing_df = housing_df.astype({'housing_median_age':int, 'total_rooms':int,
 img_california = mpimg.imread('california.png')
 
 plt.figure(figsize=(8.5,6.5))
+# longtitude is the horizontal/ latitude is the vertical position
+# Three Aesthatics : Size, color, x-Y Value
 ax = plt.scatter(housing_df.longitude, housing_df.latitude,
             s=housing_df.population/1000,
             c=housing_df.median_house_value,
             cmap='jet',
             alpha=0.25 )
+
 plt.legend( *ax.legend_elements("sizes",num=5),title='Population x 1000')
 plt.colorbar(pad=0.01).set_label(label='Median House Value', size=12)
 plt.clim(0, 550000)
 
+
+#Extend to show to correct position
 plt.imshow(img_california, extent=[-124.55, -113.80, 32.45, 42.05], alpha=0.8)
 plt.xlabel("Longitude", fontsize=12)
 plt.ylabel("Latitude", fontsize=12)
